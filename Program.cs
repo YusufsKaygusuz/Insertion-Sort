4d1c20edf5626622f4914713d198ad4786f18f47
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2022-05-05 21:52:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[] { 12,8,27,2,6,19,10};
+
+            for (int i=0; i<Numbers.Length; i++)
+            {
+                int temp = Numbers[i];
+                int j = i;
+
+                while (j > 0 && Numbers[j-1]>temp)
+                {
+                    Numbers[j] = Numbers[j - 1];
+                    j--;
+                }
+                Numbers[j] = temp;
+            }
+
+            for(int i = 0; i< Numbers.Length; i++)
+            {
+                Console.WriteLine(Numbers[i]);
+            }
+            Console.ReadLine();
+        
+        }
+
+
+    }
+}
